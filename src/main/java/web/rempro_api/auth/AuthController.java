222cc1ac0bcf41b51f6f5d4bf66409aa8093eb91
Sim <brhulla@gmail.com>
2024-10-21 17:00:48 +0200

    🎨 Swagger
    update swagger

--- src/main/java/web/rempro_api/auth/AuthController.java
+++ src/main/java/web/rempro_api/auth/AuthController.java
@@ -4,31 +4,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
 import lombok.RequiredArgsConstructor;
 import web.rempro_api.utils.dto.AuthResponse;
 import web.rempro_api.utils.dto.LoginResquest;
 import web.rempro_api.utils.dto.RegisterRequest;
-
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequestMapping("/auth")
 @Tag(name = "Auth", description = "API routes for managing authentications")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
+
     private final AuthService authService;
 
+    @Operation(summary = "Login", description = "Authenticates a user and returns a JWT token.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login successful"),
+            @ApiResponse(responseCode = "400", description = "Invalid username or password"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping(value = "login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginResquest request) {
         AuthResponse response = authService.login(request);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Register", description = "Registers a new user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User registered successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping(value = "register")
     public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
         authService.register(request);
